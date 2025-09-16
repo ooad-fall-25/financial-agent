864f60c4e2828f2b7d4bf963ff475ae5e66f9cb7
iamvann <sovannsean9@gmail.com>
2025-09-17 04:50:39 +0800

    feat(sheet seletor): add option seletors

--- src/modules/markets/ui/components/ask-ai-news-sheet.tsx
+++ src/modules/markets/ui/components/ask-ai-news-sheet.tsx
@@ -1,28 +1,127 @@
+"use client";
+
 import {
     Sheet,
     SheetContent,
     SheetDescription,
     SheetHeader,
     SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+} from "@/components/ui/sheet";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react";
+import { Label } from "@/components/ui/label";
 
 interface Props {
     isOpen: boolean;
     setIsOpen: (open: boolean) => void;
 }
 
+// TODO:consider putting these in db
+const providers = [
+    {
+        name: "Finnhub",
+        category: ["General", "Forex", "Crypto", "Merger"],
+        dayOptions: ["1", "2", "3"]
+    },
+    {
+        name: "Polygon",
+        category: ["Stock"],
+        dayOptions: ["1"]
+    }
+]
+
 export const AskAINewsSheet = ({ isOpen, setIsOpen }: Props) => {
+    const [providerName, setProviderName] = useState<string | null>(null);
+    const [category, setCategory] = useState<string | null>(null);
+    const [dayOptions, setDayOptions] = useState<string | null>(null);
+
+    // TODO: consider simplifying: use setIsOpen directly with Sheet, no need to reset the state 
+    const handleOpenChange = (open: boolean) => {
+        setIsOpen(open);
+        if (!open) {
+            setProviderName(null);
+            setCategory(null);
+            setDayOptions(null);
+        }
+    };
+
     return (
-        <Sheet open={isOpen} defaultOpen={isOpen} onOpenChange={setIsOpen}>
+        <Sheet open={isOpen} defaultOpen={isOpen} onOpenChange={handleOpenChange}>
             <SheetContent>
                 <SheetHeader>
-                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                    <SheetTitle>News Reporter</SheetTitle>
                     <SheetDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                        Select the options below and gain more insight on financial news
                     </SheetDescription>
                 </SheetHeader>
+
+                <div className="px-4 gap-y-4 flex flex-col">
+                    <Select onValueChange={(value) => setProviderName(value)}>
+                        <div className="flex justify-between">
+                            <Label>Provider</Label>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Select a provider" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Providers</SelectLabel>
+                                    {providers.map((item) => (
+                                        <SelectItem key={item.name} value={item.name.toLowerCase()} className="cursor-pointer">{item.name}</SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </div>
+                    </Select>
+
+                    {providerName &&
+                        <div className="gap-y-4 flex flex-col">
+                            <Select onValueChange={(value) => setCategory(value)}>
+                                <div className="flex justify-between">
+                                    <Label>Category</Label>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Select a category" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectLabel>Category</SelectLabel>
+                                            {providers.find((e) => e.name.toLowerCase() == providerName.toLowerCase())?.category.map((item) => (
+                                                <SelectItem key={item} value={item.toLowerCase()} className="cursor-pointer">{item}</SelectItem>
+                                            ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </div>
+                            </Select>
+
+                            <Select onValueChange={(value) => setDayOptions(value)}>
+                                <div className="flex justify-between">
+                                    <Label>Day</Label>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Select a provider" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectLabel>Day</SelectLabel>
+                                            {providers.find((e) => e.name.toLowerCase() == providerName.toLowerCase())?.dayOptions.map((item) => (
+                                                <SelectItem key={item} value={item} className="cursor-pointer">Last {item} day(s)</SelectItem>
+                                            ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </div>
+                            </Select>
+                        </div>
+                    }
+                </div>
+
+
+
             </SheetContent>
         </Sheet>
     )
