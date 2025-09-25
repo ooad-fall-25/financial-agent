97f0319db7eec1e9da70b23d0325e6319a0adbc3
iamvann <sovannsean9@gmail.com>
2025-09-25 16:38:57 +0800

    feat(library-table): add table for summary by individual link

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -3,17 +3,31 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 
 export const libraryRouter = createTRPCRouter({
-    getAllSummaryByCategory: protectedProcedure.query(async ({ctx}) => {
-        const data = await prisma.newsSummary.findMany({
-            where: {
-                userId: ctx.auth.userId,
-                url: null, 
-                isByCategory: true,
-            }
-        })
-        if (!data) {
-            throw new TRPCError({code: "NOT_FOUND", message: "No summary found"})
-        }
-        return data;
-    })
-})
+  getAllSummaryByCategory: protectedProcedure.query(async ({ ctx }) => {
+    const data = await prisma.newsSummary.findMany({
+      where: {
+        userId: ctx.auth.userId,
+        url: null,
+        isByCategory: true,
+      },
+    });
+    if (!data) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "No summary found" });
+    }
+    return data;
+  }),
+
+  getAllSummaryByIndividualLink: protectedProcedure.query(async ({ ctx }) => {
+    const data = await prisma.newsSummary.findMany({
+      where: {
+        userId: ctx.auth.userId,
+        isByCategory: false,
+      },
+    });
+
+    if (!data) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "No summary found" });
+    }
+    return data;
+  }),
+});
--- src/modules/library/ui/components/library-table.tsx
+++ src/modules/library/ui/components/library-table.tsx
@@ -5,6 +5,10 @@ interface Props {
 export const LibraryTable = ({ selectedTab }: Props) => {
     if (selectedTab === "category") {
         return <SummaryByCategoryTable />
+    } else if (selectedTab === "individual") {
+        return <SummaryByIndividualLinkTable />
+    } else {
+        return; 
     }
 }
 import {
@@ -19,51 +23,6 @@ import {
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
-
-
-
-const items = [
-    {
-        id: "1",
-        name: "Alex Thompson",
-        email: "alex.t@company.com",
-        location: "San Francisco, US",
-        status: "Active",
-        balance: "$1,250.00",
-    },
-    {
-        id: "2",
-        name: "Sarah Chen",
-        email: "sarah.c@company.com",
-        location: "Singapore",
-        status: "Active",
-        balance: "$600.00",
-    },
-    {
-        id: "3",
-        name: "James Wilson",
-        email: "j.wilson@company.com",
-        location: "London, UK",
-        status: "Inactive",
-        balance: "$650.00",
-    },
-    {
-        id: "4",
-        name: "Maria Garcia",
-        email: "m.garcia@company.com",
-        location: "Madrid, Spain",
-        status: "Active",
-        balance: "$0.00",
-    },
-    {
-        id: "5",
-        name: "David Kim",
-        email: "d.kim@company.com",
-        location: "Seoul, KR",
-        status: "Active",
-        balance: "-$1,000.00",
-    },
-]
 
 const SummaryByCategoryTable = () => {
     const trpc = useTRPC();
@@ -96,4 +55,33 @@ const SummaryByCategoryTable = () => {
     )
 }
 
+const SummaryByIndividualLinkTable = () => {
+    const trpc = useTRPC();
+    // TODO: consider using useInfiniteQuery
+    const { data } = useQuery(trpc.library.getAllSummaryByIndividualLink.queryOptions());
+    return (
+        <div>
+            <Table>
+                <TableHeader>
+                    <TableRow className="hover:bg-transparent">
+                        <TableHead>Headline</TableHead>
+                        <TableHead>Category</TableHead>
+                        <TableHead className="text-right">Created Date</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {data?.map((item) => (
+                        <TableRow key={item.id}>
+                            <TableCell className="font-medium">{item.headline}</TableCell>
+                            <TableCell className="font-medium">{item.category}</TableCell>
+                            <TableCell className="text-right">
+                                {format(item.createdAt, "HH:mm 'on' MMM dd, yyyy")}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
 
+            </Table>
+        </div>
+    )
+}
