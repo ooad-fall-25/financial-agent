8a67f4f8dfaf614d320380b81aa0cc143d218a63
iamvann <sovannsean9@gmail.com>
2025-09-25 16:16:44 +0800

    chore(db): new news summary table (with headline and isByCategory)

--- prisma/migrations/20250924194213_add_healine_and_is_by_category_column_to_news_summary/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "public"."NewsSummary" ADD COLUMN "headline" TEXT NOT NULL, 
-ADD COLUMN "isByCategory" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250925081545_news_summary_with_headline/migration.sql
+++ prisma/migrations/20250925081545_news_summary_with_headline/migration.sql
@@ -2,11 +2,13 @@
 CREATE TABLE "public"."NewsSummary" (
     "id" SERIAL NOT NULL,
     "userId" TEXT NOT NULL,
+    "headline" TEXT NOT NULL,
     "aiRepsonse" TEXT NOT NULL,
     "provider" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "language" TEXT NOT NULL,
-    "days" TEXT NOT NULL,
+    "url" TEXT,
+    "isByCategory" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
