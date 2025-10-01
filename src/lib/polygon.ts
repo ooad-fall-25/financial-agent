fe12e39b6c069c8ebaf62c0c5cf6ff31691e4056
Dhana LeTurtle <dhanakresnawijaya@gmail.com>
2025-10-01 17:16:16 +0800

    feat(news-aggr): alpaca news

--- src/lib/polygon.ts
+++ src/lib/polygon.ts
@@ -1,21 +1,47 @@
-import { restClient } from '@polygon.io/client-js';
-
+import { restClient } from "@polygon.io/client-js";
 
 const getRestClient = () => {
-        const rest = restClient(process.env.POLYGON_API_KEY || "", process.env.POLYGON_URL);
-        return rest;
-}
+  const rest = restClient(
+    process.env.POLYGON_API_KEY || "",
+    process.env.POLYGON_URL
+  );
+  return rest;
+};
 
 export const getStockNews = async (limit?: number) => {
-    const rest = getRestClient();
-    const response = await rest.listNews(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, limit);
-    return response;
-}
-
-export const getPolygonCompanyNews = async (input: string, limit?: number) => {
-  const restClient = getRestClient();
-  const response = await restClient.listNews(input, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, limit);
+  const rest = getRestClient();
+  const response = await rest.listNews(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    limit
+  );
   return response;
 };
 
-
+export const getPolygonCompanyNews = async (input: string, limit?: number) => {
+  const restClient = getRestClient();
+  const response = await restClient.listNews(
+    input,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    limit
+  );
+  return response;
+};
