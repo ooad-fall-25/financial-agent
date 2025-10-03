b278b2832f4a41a2b1c390adba43927de204d413
iamvann <sovannsean9@gmail.com>
2025-10-04 00:56:42 +0800

    style(lib): make deatail lib page relative/absolute

--- src/modules/library/ui/views/library-detail-view.tsx
+++ src/modules/library/ui/views/library-detail-view.tsx
@@ -7,6 +7,8 @@ import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { NewsDetail } from "../components/news-detail";
 import { LibraryDetailAction } from "../components/library-detail-action";
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
 
 
 
@@ -40,36 +42,53 @@ export const LibraryDetailView = ({ newsId }: Props) => {
     };
     return (
 
-        <div className="flex flex-col w-full h-screen">
-            <LibraryDetailHeader route="/library" name="Library" />
+        <div className="relative h-full w-full min-h-0">
+            <div
+                className="absolute top-0 left-0 right-0 z-20 bg-background border-b border-primary p-4"
+                role="banner"
+            >
+                <Button asChild variant="ghost" className="hover:border">
+                    <Link href={`/library`} className="flex items-center gap-2">
+                        <ArrowLeftIcon /> Library
+                    </Link>
+                </Button>
+            </div>
 
             {news ? (
-                <div className="grid grid-cols-8 flex-1 min-h-0 text-sm">
-                    <div className="col-span-5 p-8 border-r border-primary overflow-y-auto">
-                        <div className="flex items-center justify-between">
-                            <h1 className="text-muted-foreground">
-                                AI generated summary report
-                            </h1>
-                            <Button variant="outline" onClick={() => handleDownload(news.aiRepsonse)}>
-                                <span>Download as PDF</span>
-                            </Button>
-                        </div>
-                        <div className="p-8">
-                            <AIResponse>
-                                {news.aiRepsonse}
-                            </AIResponse>
-                        </div>
-                    </div>
+                <div className="absolute top-0 bottom-0 left-0 right-0 pt-16 min-h-0">
+                    <div className="grid grid-cols-8 h-full min-h-0 text-sm">
 
-                    <div className="col-span-3 overflow-y-auto py-8">
-                        <div className="flex flex-col px-4 pb-8 border-b border-primary">
-                            <NewsDetail news={news} />
+                        <div className="col-span-5 flex flex-col min-h-0 border-r border-primary">
+                            <div className="flex-1 overflow-y-auto min-h-0 p-8">
+                                <div className="flex items-center justify-between">
+                                    <h1 className="text-muted-foreground">
+                                        AI generated summary report
+                                    </h1>
+                                    <Button variant="outline" onClick={() => handleDownload(news.aiRepsonse)}>
+                                        <span>Download as PDF</span>
+                                    </Button>
+                                </div>
+                                <div className="p-8">
+                                    <AIResponse>
+                                        {news.aiRepsonse}
+                                    </AIResponse>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <LibraryDetailAction newsId={news.id} />
+
+                        <div className="col-span-3 flex flex-col min-h-0">
+                            <div className="flex-1 overflow-y-auto min-h-0">
+                                <div className="flex flex-col px-4 py-8 border-b border-primary">
+                                    <NewsDetail news={news} />
+                                </div>
+                                <div>
+                                    <LibraryDetailAction newsId={news.id} />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             ) : (
                 <div className="flex-1 flex items-center justify-center">
                     No AI Summary
